02181963bc082df6debb82dc1bff25f954b38dc9
Mphaga Philelo <mphagaphilelo432@gmail.com>
2026-07-20 10:54:28 +0200

    adding dim_payment stg table

---
+++ sql_scripts/creating_stg_dim_tables/01_dim_payment.sql
@@ -0,0 +1,21 @@
+--creating stg dim_payment
+
+if object_id ('[stg_bright_retail].[dbo].[dim_payment]', 'U') is null
+begin
+    create table [stg_bright_retail].[dbo].[dim_payment] (
+        payment_id int identity(1,1) primary key,
+        payment_method varchar(250) 
+    );
+end
+go
+
+--inserting the data from the raw dataset to the dim_payment
+    insert into [stg_bright_retail].[dbo].[dim_payment] (payment_method)
+
+    select distinct payment_method
+    from [stg_bright_retail].[dbo].[bright_retail_raw_data];
+go
+
+--verifying if the insert worked
+select *
+from [stg_bright_retail].[dbo].[dim_payment];
