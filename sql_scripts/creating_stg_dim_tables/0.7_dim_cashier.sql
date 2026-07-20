b2a0d15217eb254b9785507e11c6e47f9958400f
Mphaga Philelo <mphagaphilelo432@gmail.com>
2026-07-20 15:03:58 +0200

    adding stg_dim_cashier table script

---
+++ sql_scripts/creating_stg_dim_tables/0.7_dim_cashier.sql
@@ -0,0 +1,27 @@
+drop table if exists [stg_bright_retail].[dbo].[dim_cashier];
+go
+
+if object_id('[stg_bright_retail].[dbo].[dim_cashier]', 'u') is null
+begin
+    create table [stg_bright_retail].[dbo].[dim_cashier] (
+        cashier_id         int identity(1,1) primary key,
+        cashier_name   varchar(250) not null
+    );
+end
+go
+
+--truncate table
+truncate table [stg_bright_retail].[dbo].[dim_cashier]
+
+insert into [stg_bright_retail].[dbo].[dim_cashier] (cashier_name)
+
+select distinct 
+        cashier_name
+from stg_bright_retail.dbo.bright_retail_raw_data
+;
+go
+
+
+--verify the insert
+select *
+from [stg_bright_retail].[dbo].[dim_cashier] 
